4412b52e88983d727102f6dd07248456cf4f97dd
pranayjain75 <pranayjain75@gmail.com>
2021-03-26 19:04:21 -0400

    Bukwild

--- src/components/MarqueeComponent.js
+++ src/components/MarqueeComponent.js
@@ -1,26 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./component.scss";
 import CTAComponent from "./CTAComponent";
-// import backgroundImage from "../assets"
+import industries from "../assets/slide_one.jpg";
+
+import services from "../assets/slide_two.jpg";
+
+import aboutus from "../assets/slide_three.jpg";
+
 
 const MarqueeComponent = (props) => {
+
+
+
   const currentMarquee = props.pageContent.filter((item) => {
     return item.title == props.currentPage;
   });
   console.log("Hello", currentMarquee);
-  if (currentMarquee.length > 0) {
-    return (
 
+
+  if (currentMarquee.length > 0) {
+    let imgUrl;
+    switch (currentMarquee[0].blocks[0].background) {
+      case "slide_one.jpg":
+        imgUrl = industries;
+        break;
+      case "slide_two.jpg":
+        imgUrl = services;
+        break;
+      case "slide_three.jpg":
+        imgUrl = aboutus;
+        break;
+    }
+    return (
       <div
         className="marquee-container"
         style={{
-          backgroundImage: `url(${"../assets/currentMarquee[0].blocks[0].background"})`,
+          backgroundImage: `url('${imgUrl}')`,
           backgroundRepeat: "no-repeat",
           backgroundSize: "cover",
           backgroundColor: "#d4d4d4",
         }}
       >
-        <div className="container ">
+        < div className="container " >
           <div className="heading-container">
             <p className="heading">{currentMarquee[0].blocks[0].headline}</p>
           </div>
@@ -29,8 +50,8 @@ const MarqueeComponent = (props) => {
           </div>
           <CTAComponent text={currentMarquee[0].blocks[0].cta} img={""} />
 
-        </div>
-      </div>
+        </div >
+      </div >
     );
   } else {
     return null;
